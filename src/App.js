d137f05add8478f7c15ce743cfcebc008c9efe62
Meen Philip <meenphilip@gmail.com>
2022-08-16 18:48:18 +0300

    Intial React files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello React on my own</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
