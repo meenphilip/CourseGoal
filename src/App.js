d6793e48b7fb055c0e71f70963d007b77bef0306
Meen Philip <meenphilip@gmail.com>
2022-08-20 13:40:08 +0300

    add CourseInput & CourseGoalList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import "./App.css";
+import CourseGoalList from "./components/CourseGaol/CourseGoalList/CourseGoalList";
+import CourseInput from "./components/CourseGaol/CourseInput/CourseInput";
 
 function App() {
+  const courseItems = [
+    {
+      id: "g1",
+      text: "Do all course exercise!",
+    },
+    {
+      id: "g2",
+      text: "Finish the course!",
+    },
+    {
+      id: "g3",
+      text: "Build the UI by myself",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello React on my own</p>
-      </header>
+      <section id="goal-form">
+        <CourseInput />
+      </section>
+      <section id="goals">
+        <CourseGoalList item={courseItems} />
+      </section>
     </div>
   );
 }
