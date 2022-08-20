e96a2bf7d9ad7cb5bdb48e5b274bc5a008a25c3f
Meen Philip <meenphilip@gmail.com>
2022-08-20 14:53:17 +0300

    Use state to manage user input

--- src/components/CourseGaol/CourseInput/CourseInput.jsx
+++ src/components/CourseGaol/CourseInput/CourseInput.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
 
 function CourseInput() {
+  const [enteredText, setEnteredText] = useState("");
+
+  const addNewCourseGoalHandler = (event) => {
+    setEnteredText(event.target.value);
+  };
+
+  const onSaveHandler = (event) => {
+    event.preventDefault();
+
+    // combine the input into an object
+    const courseGoalData = {
+      text: enteredText,
+    };
+
+    console.log(courseGoalData);
+
+    setEnteredText("");
+  };
   return (
-    <form>
+    <form onSubmit={onSaveHandler}>
       <div className="form-control">
         <label>Course Goal</label>
-        <input type="text" />
+        <input type="text" value={enteredText} onChange={addNewCourseGoalHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
