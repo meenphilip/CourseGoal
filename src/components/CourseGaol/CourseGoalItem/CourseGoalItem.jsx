1ad5fa443f69417b1215f897c9d1cc9ec1708db4
Meen Philip <meenphilip@gmail.com>
2022-08-24 14:59:24 +0300

    Add ondelete prop

--- src/components/CourseGaol/CourseGoalItem/CourseGoalItem.jsx
+++ src/components/CourseGaol/CourseGoalItem/CourseGoalItem.jsx
@@ -1,11 +1,10 @@
 import "./CourseGoalItem.css";
 
 const CourseGoalItem = (props) => {
-
   const deleteHandler = () => {
     props.onDelete(props.id);
   };
-  
+
   return (
     <li className="goal-item " onClick={deleteHandler}>
       {props.children}
